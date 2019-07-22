704c7545e69e53e04b175033c566fd1ab8ab26ca
LibraTang <1026960839@qq.com>
2019-07-22 22:21:03 +0800

    initialize

---
+++ src/main/java/com/suncaper/hotel_manager/HotelManagerApplication.java
@@ -0,0 +1,13 @@
+package com.suncaper.hotel_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelManagerApplication.class, args);
+    }
+
+}
