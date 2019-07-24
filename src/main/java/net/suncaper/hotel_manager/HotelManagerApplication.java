08129346dd4fc7b0a4efa3024523b67e1921ad93
jrk <1377437271@qq.com>
2019-07-24 15:16:08 +0800

    login

--- src/main/java/net/suncaper/hotel_manager/HotelManagerApplication.java
+++ src/main/java/net/suncaper/hotel_manager/HotelManagerApplication.java
@@ -1,9 +1,11 @@
 package net.suncaper.hotel_manager;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("net.suncaper.hotel.manager.mapper")
 public class HotelManagerApplication {
 
     public static void main(String[] args) {
--- src/main/java/net/suncaper/hotel_manager/service/UserService.java
+++ src/main/java/net/suncaper/hotel_manager/service/UserService.java
@@ -1,4 +1,17 @@
 package net.suncaper.hotel_manager.service;
 
+import net.suncaper.hotel_manager.domain.H_User;
+import net.suncaper.hotel_manager.mapper.H_UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    private H_UserMapper h_userMapper;
+
+    public int getIdByAccount(String u_account,String u_password){
+        H_User h_user = h_userMapper.selectByAccount(u_account,u_password);
+        return h_user!=null ? h_user.getuId() : -1;
+    }
 }
