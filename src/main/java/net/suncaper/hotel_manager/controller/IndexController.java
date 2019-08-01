1cb49da095bf5a1bc970a6d43b1f4e8a39936e72
jrk <1377437271@qq.com>
2019-08-01 16:07:11 +0800

    index链接后端

--- src/main/java/net/suncaper/hotel_manager/controller/IndexController.java
+++ src/main/java/net/suncaper/hotel_manager/controller/IndexController.java
@@ -6,5 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     @RequestMapping("")
-    public String index() {return "user/index";}
+    public String index() {
+
+        return "user/index";
+    }
+
 }
