306558f554e24ecb3bb08c3a2ea7cf893538dbcc
LibraTang <1026960839@qq.com>
2019-08-01 17:08:46 +0800

    首页显示五星级酒店和评分最高酒店

--- src/main/java/net/suncaper/hotel_manager/controller/IndexController.java
+++ src/main/java/net/suncaper/hotel_manager/controller/IndexController.java
@@ -1,14 +1,23 @@
 package net.suncaper.hotel_manager.controller;
 
+import net.suncaper.hotel_manager.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-    @RequestMapping("")
-    public String index() {
+    @Autowired
+    HotelService hotelService;
 
-        return "user/index";
+    @RequestMapping("")
+    public ModelAndView index() {
+        ModelAndView mav = new ModelAndView("user/index");
+        mav.addObject("fiveStarHotels", hotelService.selectFiveStarHotel());
+        mav.addObject("topRatedHotels", hotelService.selectTopRatedHotel());
+
+        return mav;
     }
 
 }
