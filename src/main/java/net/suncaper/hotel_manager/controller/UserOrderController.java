60862dbfd3d14fc7fcd313b5e9e1002c04ea0d45
jrk <1377437271@qq.com>
2019-08-07 11:49:47 +0800

    修改admin顶栏、admin左上角、user和admin登陆注册全部输入完成判断

--- src/main/java/net/suncaper/hotel_manager/controller/UserOrderController.java
+++ src/main/java/net/suncaper/hotel_manager/controller/UserOrderController.java
@@ -178,8 +178,7 @@ public class UserOrderController {
         //取消订单，置订单状态为3
         orderService.cancelOrder(o_id, session.getId());
         //更新房间状态
-        orderService.leaveRoom(h_r
-                oom);
+        orderService.leaveRoom(h_room);
 
         return "redirect:/user/orderList";
     }
